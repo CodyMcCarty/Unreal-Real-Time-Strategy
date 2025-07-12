46d3f741ed86a1b0bb7df20ce204427a9263b6d5
Cody McCarty <codyjmccarty@gmail.com>
2025-07-12 02:20:07 -0500

    Camera movement

---
+++ Source/UE_RTS/Player/StratPlayerCameraPawn.cpp
@@ -0,0 +1,340 @@
+﻿// Copyright Cody McCarty.
+
+#include "StratPlayerCameraPawn.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "KismetTraceUtils.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Net/UnrealNetwork.h"
+
+namespace
+{
+	float GetZoomAlpha(const float TargetArmLength, const float MinZoom, const float MaxZoom)
+	{
+		return (TargetArmLength - MinZoom) / (MaxZoom - MinZoom);
+	}
+
+	void SetInputMode_RTSStyle(APlayerController* PC)
+	{
+		check(PC)
+		FInputModeGameAndUI GameAndUIInputMode;
+		GameAndUIInputMode.SetHideCursorDuringCapture(false);
+		GameAndUIInputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+		PC->SetInputMode(GameAndUIInputMode);
+		PC->SetShowMouseCursor(true);
+	}
+
+	FRotator ClampPitch(const FRotator& Rot)
+	{
+		FRotator Result = Rot;
+		Result.Pitch = FMath::Clamp(Result.Pitch, -85.f, 10.f);
+		return Result;
+	}
+
+	bool GetMousePos(const APlayerController* PC, FIntPoint& OutMousePos)
+	{
+		if (PC)
+		{
+			if (const ULocalPlayer* Player = PC->GetLocalPlayer())
+			{
+				if (const UGameViewportClient* ViewportClient = Player->ViewportClient)
+				{
+					if (FViewport* Viewport = ViewportClient->Viewport)
+					{
+						Viewport->GetMousePos(OutMousePos);
+						if (OutMousePos.X >= 0 && OutMousePos.Y >= 0)
+						{
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	bool SetMousePos(const APlayerController* PC, const FIntPoint& MousePos)
+	{
+		if (PC)
+		{
+			if (const ULocalPlayer* Player = PC->GetLocalPlayer())
+			{
+				if (const UGameViewportClient* ViewportClient = Player->ViewportClient)
+				{
+					if (FViewport* Viewport = ViewportClient->Viewport)
+					{
+						Viewport->SetMouse(MousePos.X, MousePos.Y);
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
+
+AStratPlayerCameraPawn::AStratPlayerCameraPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetReplicatingMovement(false);
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	SpringArmComp->bDoCollisionTest = false;
+	SpringArmComp->CameraLagSpeed = CameraLagSpeed;
+	SpringArmComp->CameraRotationLagSpeed = CameraRotationLagSpeed;
+	SpringArmComp->bEnableCameraLag = false;
+	SpringArmComp->bEnableCameraRotationLag = false;
+	SpringArmComp->TargetArmLength = TargetArmLength;
+	SpringArmComp->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+
+	const float ArmLengthNormal = GetZoomAlpha(TargetArmLength, MinZoom, MaxZoom);
+	MoveSpeed = FMath::Lerp(MinMoveSpeed, MaxMoveSpeed, ArmLengthNormal);
+}
+
+void AStratPlayerCameraPawn::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(AStratPlayerCameraPawn, SimpleReplicatedMovement, COND_SkipOwner);
+}
+
+void AStratPlayerCameraPawn::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
+void AStratPlayerCameraPawn::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	const ULocalPlayer* LocalPlayer = CastChecked<APlayerController>(Controller)->GetLocalPlayer();
+	UEnhancedInputLocalPlayerSubsystem* EnhancedInputSS = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+	check(EnhancedInputSS);
+
+	if (ensure(CameraMovementInputMapping))
+	{
+		EnhancedInputSS->AddMappingContext(CameraMovementInputMapping, 1);
+	}
+
+	UEnhancedInputComponent* EnhancedInputComp = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInputComp->BindAction(Input_Move, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	EnhancedInputComp->BindAction(Input_Zoom, ETriggerEvent::Triggered, this, &ThisClass::Zoom);
+	EnhancedInputComp->BindAction(Input_Rotate, ETriggerEvent::Started, this, &ThisClass::RotateStarted);
+	EnhancedInputComp->BindAction(Input_Rotate, ETriggerEvent::Triggered, this, &ThisClass::Rotate);
+	EnhancedInputComp->BindAction(Input_EnableRotate, ETriggerEvent::Started, this, &ThisClass::EnableRotateStarted);
+	EnhancedInputComp->BindAction(Input_EnableRotate, ETriggerEvent::Canceled, this, &ThisClass::EnableRotateEnded);
+	EnhancedInputComp->BindAction(Input_EnableRotate, ETriggerEvent::Completed, this, &ThisClass::EnableRotateEnded);
+}
+
+void AStratPlayerCameraPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SimpleReplicatedMovement.Location = GetActorLocation();
+	SimpleReplicatedMovement.Yaw = GetActorRotation().Yaw;
+	
+	if (Controller)
+	{
+		const FRotator ControlRot = GetControlRotation();
+		const FRotator SpringArmRot = SpringArmComp->GetRelativeRotation();
+		const FRotator WithSpringArmPitch(SpringArmRot.GetDenormalized().Pitch, ControlRot.Yaw, ControlRot.Roll);
+		Controller->SetControlRotation(WithSpringArmPitch);
+	}
+}
+
+void AStratPlayerCameraPawn::NotifyControllerChanged()
+{
+	if (IsLocallyControlled())
+	{
+		GetWorldTimerManager().SetTimer(TraceForCameraHeight_TimerHandle, this, &ThisClass::TimerLoop_TraceForCameraHeight, 1 / TraceForCameraHeight_TimerFreq, true);
+
+		SetInputMode_RTSStyle(Cast<APlayerController>(Controller));
+	}
+	
+	Super::NotifyControllerChanged(); //~Super calls BP handler, and PreviousController = Controller;
+}
+
+void AStratPlayerCameraPawn::OnRep_Controller()
+{
+	Super::OnRep_Controller();
+
+	GetWorldTimerManager().SetTimer(SendSimpleRepMovement_TimerHandle, this, &ThisClass::TimerLoop_SendSimpleRepMovement, 1 / SendTransform_TimerFreq, true);
+}
+
+UE_DISABLE_OPTIMIZATION
+
+void AStratPlayerCameraPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsLocallyControlled())
+	{
+		//~ Movement
+		const FVector InputVector = ConsumeMovementInputVector();
+		const FVector Velocity = InputVector * MoveSpeed * DeltaTime;
+		SimpleReplicatedMovement.Location += Velocity;
+
+		//~ Zoom
+		SpringArmComp->TargetArmLength = FMath::FInterpTo(SpringArmComp->TargetArmLength, TargetArmLength, DeltaTime, CameraZoomLagSpeed);
+
+		//~ Rotation
+		const FRotator ControlRot = ClampPitch(GetControlRotation().GetNormalized());
+		SimpleReplicatedMovement.Yaw = ControlRot.Yaw;
+		Controller->SetControlRotation(ControlRot.GetDenormalized());
+
+		const FRotator CurrentRot(SpringArmComp->GetRelativeRotation().Pitch, GetActorRotation().Yaw, 0.f);
+		const FRotator TargetRot(FMath::QInterpTo(FQuat(CurrentRot), FQuat(ControlRot), DeltaTime, CameraRotationLagSpeed));
+
+		SpringArmComp->SetRelativeRotation(FRotator(TargetRot.Pitch, 0.f, 0.f));
+		SetActorRotation(FRotator(0.f, TargetRot.Yaw, 0.f));;
+
+		if (GetLocalRole() == ROLE_AutonomousProxy)
+		{
+		}
+	}
+	else
+	{
+		const FRotator ProxyRot(0.f, SimpleReplicatedMovement.Yaw, 0.f);
+		SetActorRotation(ProxyRot);
+	}
+
+	FVector ActorLocation = GetActorLocation();
+	FVector TargetLocation = FMath::VInterpTo(ActorLocation, SimpleReplicatedMovement.Location, DeltaTime, CameraLagSpeed);
+	SetActorLocation(TargetLocation);
+
+#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+	if (bDrawDebugMarkers)
+	{
+		if (const UWorld* World = GetWorld())
+		{
+			DrawDebugSphere(World, ActorLocation, 5.f, 8, FColor::Green);
+			DrawDebugSphere(World, SimpleReplicatedMovement.Location, 5.f, 8, FColor::Yellow);
+
+			const FVector ToOrigin = ActorLocation - SimpleReplicatedMovement.Location;
+			DrawDebugDirectionalArrow(World, SimpleReplicatedMovement.Location, SimpleReplicatedMovement.Location + ToOrigin * 0.5f, 7.5f, FColor::Green);
+			DrawDebugDirectionalArrow(World, SimpleReplicatedMovement.Location + ToOrigin * 0.5f, ActorLocation, 7.5f, FColor::Green);
+
+			DrawDebugCone(World, ActorLocation, GetControlRotation().Vector(), 125.f, 0.7f, 0.f, 8, FColor::Yellow);
+			DrawDebugCone(World, ActorLocation, GetActorForwardVector(), 175.f, 0.7f, 0.f, 8, FColor::Orange);
+			DrawDebugCone(World, ActorLocation, SpringArmComp->GetForwardVector(), 225.f, 0.7f, 0.f, 8, FColor::Red);
+		}
+	}
+#endif
+}
+
+UE_ENABLE_OPTIMIZATION
+
+void AStratPlayerCameraPawn::TimerLoop_TraceForCameraHeight()
+{
+	const UWorld* World = GetWorld();
+	if (!World) { return; }
+
+	TArray<FHitResult> HitResults;
+	const FVector Dist(0.f, 0.f, 10'000.f);
+	const FVector Start = SimpleReplicatedMovement.Location + Dist;
+	const FVector End = SimpleReplicatedMovement.Location - Dist;
+	constexpr float Radius = 50.f;
+	FCollisionQueryParams QueryParams = FCollisionQueryParams::DefaultQueryParam;
+	QueryParams.AddIgnoredActor(this);
+	QueryParams.bTraceComplex = false;
+	const bool bHit = World->SweepMultiByChannel(HitResults, Start, End, FQuat::Identity, TerrainHeightTraceChannel, FCollisionShape::MakeSphere(Radius), QueryParams);
+
+#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+	if (bDrawDebugMarkers)
+	{
+		DrawDebugSphereTraceMulti(World, Start, End, Radius, EDrawDebugTrace::ForDuration, bHit, HitResults, FLinearColor::Red, FLinearColor::Green, 1 / TraceForCameraHeight_TimerFreq);
+	}
+#endif
+
+	//~ a blocking hit (if found) will be the last element of the array. the results are [0] is top floor [last] is terrain.
+	//~ todo: have floor system. I could have terrain block, and floors overlap.
+	if (!HitResults.IsEmpty())
+	{
+		const float GroundZ = HitResults[0].Location.Z;
+		SimpleReplicatedMovement.Location.Z = GroundZ;
+	}
+}
+
+void AStratPlayerCameraPawn::TimerLoop_SendSimpleRepMovement()
+{
+	SimpleReplicatedMovement.ServerFrame++;
+	Server_SetSimpleReplicatedMovementState(SimpleReplicatedMovement);
+}
+
+void AStratPlayerCameraPawn::Move(const FInputActionInstance& InputActionInstance)
+{
+	const FVector2D Direction = InputActionInstance.GetValue().Get<FVector2D>().GetSafeNormal();
+	AddMovementInput(GetActorForwardVector(), Direction.Y);
+	AddMovementInput(GetActorRightVector(), Direction.X);
+}
+
+void AStratPlayerCameraPawn::Zoom(const FInputActionInstance& InputActionInstance)
+{
+	const float ZoomValue = InputActionInstance.GetValue().Get<float>();
+	const float ZoomDelta = FMath::Max(SpringArmComp->TargetArmLength / UE_GOLDEN_RATIO, 50.f);
+
+	TargetArmLength += ZoomDelta * ZoomValue;
+	TargetArmLength = FMath::Clamp(TargetArmLength, MinZoom, MaxZoom);
+
+	const float ArmLengthNormal = GetZoomAlpha(TargetArmLength, MinZoom, MaxZoom);
+	MoveSpeed = FMath::Lerp(MinMoveSpeed, MaxMoveSpeed, ArmLengthNormal);
+}
+
+void AStratPlayerCameraPawn::RotateStarted(const FInputActionInstance& InputActionInstance)
+{
+	APlayerController* PC = CastChecked<APlayerController>(Controller);
+	if (PC->bShowMouseCursor)
+	{
+		PC->SetShowMouseCursor(false);
+		GetMousePos(PC, MousePosSnapshot);
+		PC->SetInputMode(FInputModeGameOnly());
+	}
+}
+
+void AStratPlayerCameraPawn::Rotate(const FInputActionInstance& InputActionInstance)
+{
+	const FVector2D RotateValue = InputActionInstance.GetValue().Get<FVector2D>();
+	AddControllerYawInput(RotateValue.X);
+	AddControllerPitchInput(RotateValue.Y);
+}
+
+void AStratPlayerCameraPawn::EnableRotateStarted(const FInputActionInstance& InputActionInstance)
+{
+	APlayerController* PC = CastChecked<APlayerController>(Controller);
+	if (PC->bShowMouseCursor)
+	{
+		PC->SetShowMouseCursor(false);
+		GetMousePos(PC, MousePosSnapshot);
+		PC->SetInputMode(FInputModeGameOnly());
+	}
+}
+
+void AStratPlayerCameraPawn::EnableRotateEnded(const FInputActionInstance& InputActionInstance)
+{
+	APlayerController* PC = CastChecked<APlayerController>(Controller);
+	SetInputMode_RTSStyle(PC);
+	SetMousePos(PC, MousePosSnapshot);
+}
+
+void AStratPlayerCameraPawn::OnRep_SimpleReplicatedMovement(const FSimpleRepMovement& OldSimpleRepMovement)
+{
+	check(!IsLocallyControlled());
+	if (OldSimpleRepMovement.ServerFrame > SimpleReplicatedMovement.ServerFrame)
+	{
+		SimpleReplicatedMovement = OldSimpleRepMovement;
+	}
+}
+
+// todo: can\should I pass a ref?
+void AStratPlayerCameraPawn::Server_SetSimpleReplicatedMovementState_Implementation(const FSimpleRepMovement NewSimpleRepMovement)
+{
+	if (NewSimpleRepMovement.ServerFrame > SimpleReplicatedMovement.ServerFrame)
+	{
+		SimpleReplicatedMovement = NewSimpleRepMovement;
+	}
+}
---
+++ Source/UE_RTS/Player/StratPlayerCameraPawn.h
@@ -0,0 +1,161 @@
+﻿// Copyright Cody McCarty.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "ModularPawn.h"
+#include "StratPlayerCameraPawn.generated.h"
+
+class USpringArmComponent;
+class UInputAction;
+class UInputMappingContext;
+
+/** Replicated movement data. Simplified version of FRepMovement more suited to an RTS camera.  */
+USTRUCT()
+struct FSimpleRepMovement
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Transient)
+	FVector Location{FVector::ZeroVector};
+
+	UPROPERTY(Transient)
+	float Yaw{0};
+
+	/** Is used to compare out of data packets. We only want the most recent transform. Increment this before sending. */
+	UPROPERTY(Transient)
+	uint32 ServerFrame{0};
+};
+
+/** Responsible for moving the player around the map. */
+UCLASS()
+class UE_RTS_API AStratPlayerCameraPawn : public AModularPawn
+{
+	GENERATED_BODY()
+#pragma region Lifecycle
+
+public:
+	AStratPlayerCameraPawn();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PostInitializeComponents() override;
+
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void BeginPlay() override;
+
+public:
+	virtual void NotifyControllerChanged() override;
+	// virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_Controller() override;
+	// virtual void OnRep_PlayerState() override;
+	// virtual void OnPlayerStateChanged(APlayerState* NewPlayerState, APlayerState* OldPlayerState) override;
+	virtual void Tick(float DeltaTime) override;
+#pragma endregion
+
+protected:
+	void TimerLoop_TraceForCameraHeight();
+	void TimerLoop_SendSimpleRepMovement();
+	void Move(const FInputActionInstance& InputActionInstance);
+	void Zoom(const FInputActionInstance& InputActionInstance);
+	void RotateStarted(const FInputActionInstance& InputActionInstance);
+	void Rotate(const FInputActionInstance& InputActionInstance);
+	void EnableRotateStarted(const FInputActionInstance& InputActionInstance);
+	void EnableRotateEnded(const FInputActionInstance& InputActionInstance);
+
+	UPROPERTY(VisibleInstanceOnly, Category="UserOptions|Info", ReplicatedUsing=OnRep_SimpleReplicatedMovement)
+	FSimpleRepMovement SimpleReplicatedMovement;
+
+	UFUNCTION()
+	void OnRep_SimpleReplicatedMovement(const FSimpleRepMovement& OldSimpleRepMovement);
+
+	UFUNCTION(Server, Unreliable)
+	void Server_SetSimpleReplicatedMovementState(FSimpleRepMovement NewSimpleRepMovement);
+
+	UPROPERTY() // VisibleAnywhere, BlueprintReadOnly, Category="Components|Native"
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+
+	FTimerHandle SendSimpleRepMovement_TimerHandle;
+	FTimerHandle TraceForCameraHeight_TimerHandle;
+	FIntPoint MousePosSnapshot;
+
+	/** Draws a trace for the ground. Draws Rotations (Control=Yellow, Actor=Orange, SpringArm=Red). Draws markers at the camera target (in green) and the lagged position (in yellow). A line is drawn between the two locations, in green. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, AdvancedDisplay, Category=UserOptions)
+	bool bDrawDebugMarkers{false};
+
+	/** This value is calculated. Based on Zoom, Min&MaxZoom, and Min&MaxMoveSpeed. Zoomed out goes faster. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="UserOptions|Info", meta=(Units="cm/s"))
+	float MoveSpeed{800.f};
+
+	/** Zoom. The length of the SpringArm after interpolation. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="UserOptions|Info", meta=(Units="cm"))
+	float TargetArmLength{800.f};
+	
+#pragma region BPConfig
+
+protected:
+	/** If supplied, will be added with 1 priority. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions)
+	TObjectPtr<UInputMappingContext> CameraMovementInputMapping;
+
+	/** Expects an Axis2D (Vector2D). Normally WASD or ESDF */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions)
+	TObjectPtr<UInputAction> Input_Move;
+
+	/** Expects Axis1D (float). Normally mouse wheel axis in/out */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions)
+	TObjectPtr<UInputAction> Input_Zoom;
+
+	/** Expects an Axis2D (Vector2D). Normally mouse XY  */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions)
+	TObjectPtr<UInputAction> Input_Rotate;
+
+	/** Expects a bool. Normally mmb and/or ctrl, alt */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions)
+	TObjectPtr<UInputAction> Input_EnableRotate;
+
+	/** Controls how quickly the camera reaches target position. Low values are slower (more lag), high values are faster (less lag), while zero is instant (no lag). */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=UserOptions, meta=(ClampMin="0.0", ClampMax="1000.0", UIMin = "0.0", UIMax = "50.0"))
+	float CameraLagSpeed{5.f};
+
+	/** Zoom adjusts speed. This is the speed when zoomed all the way in. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=UserOptions, meta=(ClampMin="50.0", UIMin="200.0", UIMax="4000.0", Units="cm/s"))
+	float MinMoveSpeed{1100.f};
+
+	/** Zoom adjusts speed. This is the speed when zoomed all the way out. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=UserOptions, meta=(ClampMin="50.0", UIMin="1000.0", UIMax="30000.0", Units="cm/s"))
+	float MaxMoveSpeed{15000.f};
+
+	/** This also affects speed */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=UserOptions, meta=(ClampMin="50.0", UIMin="50.0", UIMax="500.0", Units="cm"))
+	float MinZoom{200.f};
+
+	/** This also affects speed */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=UserOptions, meta=(ClampMin="50.0", UIMin="1000.0", UIMax="15000.0", Units="cm"))
+	float MaxZoom{5000.f};
+
+	/** Controls how quickly the camera reaches target position. Low values are slower (more lag), high values are faster (less lag), while zero is instant (no lag). */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=UserOptions, meta=(ClampMin="0.0", UIMin="0.0", UIMax="10.0"))
+	float CameraZoomLagSpeed{5.f};
+
+	/** Controls how quickly the camera reaches target position. Low values are slower (more lag), high values are faster (less lag), while zero is instant (no lag). */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Lag, meta=(ClampMin="0.0", ClampMax="1000.0", UIMin = "0.0", UIMax = "50.0"))
+	float CameraRotationLagSpeed{7.f};
+
+	/** Used to make non-human pawns move more smoothly. Lower values are slower. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions, AdvancedDisplay, meta=(ClampMin=0.01f, UIMin=0.01f, UIMax=2.f, Units="times"))
+	float ProxyCameraLagSpeedMultiplier{0.33f};
+
+	/** How often the client player's location is sent to the server in FPS. Lower for better performance. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions, AdvancedDisplay, meta=(ClampMin=0.1f, ClampMax=60.f, UIMin=1.f, UIMax=30.f, Units="times"))
+	float SendTransform_TimerFreq{3.f};
+
+	/** How often the locally controlled pawn's height is updated in FPS. Lower for better performance. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=UserOptions, AdvancedDisplay, meta=(ClampMin=0.1f, ClampMax=60.f, UIMin=1.f, UIMax=30.f, Units="times"))
+	float TraceForCameraHeight_TimerFreq{5.f};
+
+	/** The channel used to set the height. Should check against the ground or building floors so we don't fall through */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=UserOptions)
+	TEnumAsByte<ECollisionChannel> TerrainHeightTraceChannel{ECC_Visibility};
+#pragma endregion
+};
