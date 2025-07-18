667a0375ea9cecc79edda24ea933ed0ad6ca687e
Cody McCarty <codyjmccarty@gmail.com>
2025-07-17 23:44:01 -0500

    PlayerStat::PlayerColor replicated.

---
+++ Source/UE_RTS/Player/StratPlayerState.cpp
@@ -0,0 +1,84 @@
+﻿// Copyright Cody McCarty.
+
+
+#include "StratPlayerState.h"
+
+#include "SandCoreLogToolsBPLibrary.h"
+#include "Net/UnrealNetwork.h"
+
+namespace
+{
+	FLinearColor Debug_MakeColorFromPieId(const int32 PieId)
+	{
+		FLinearColor NewPlayerColor(FMath::FRand(), FMath::FRand(), FMath::FRand(), 1.f);
+#if WITH_EDITOR
+		switch (PieId)
+		{
+		case 0: NewPlayerColor = FLinearColor::Red;
+			break;
+		case 1: NewPlayerColor = FLinearColor::Green;
+			break;
+		case 2: NewPlayerColor = FLinearColor::Blue;
+			break;
+		default: NewPlayerColor = FLinearColor::Yellow;
+		}
+#endif
+
+		return NewPlayerColor;
+	}
+}
+
+AStratPlayerState::AStratPlayerState()
+{
+}
+
+void AStratPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AStratPlayerState, PlayerColor);
+}
+
+void AStratPlayerState::BeginReplication()
+{
+	Super::BeginReplication();
+
+	const APlayerController* PC = GetPlayerController();
+	if (PC && PC->IsLocalController())
+	{
+		const FLinearColor MakeColorFromPieId = Debug_MakeColorFromPieId(UE::GetPlayInEditorID());
+		if (HasAuthority())
+		{
+			SetPlayerColor(MakeColorFromPieId);
+		}
+		else
+		{
+			Server_SetPlayerColor(MakeColorFromPieId);
+		}
+	}
+}
+
+void AStratPlayerState::SetPlayerColor(const FLinearColor& NewPlayerColor)
+{
+	if (NewPlayerColor != PlayerColor)
+	{
+		const FLinearColor OldPlayerColor = PlayerColor;
+		PlayerColor = NewPlayerColor;
+		BroadcastPlayerColorChanged(OldPlayerColor);
+	}
+}
+
+void AStratPlayerState::OnRep_PlayerColor(const FLinearColor& OldPlayerColor)
+{
+	if (PlayerColor != OldPlayerColor)
+	{
+		BroadcastPlayerColorChanged(OldPlayerColor);
+	}
+}
+
+void AStratPlayerState::BroadcastPlayerColorChanged(const FLinearColor& OldPlayerColor)
+{
+	INFO_LOG(LogTemp, Warning, TEXT("NewColor=%s  OldColor=%s"), *PlayerColor.ToString(), *OldPlayerColor.ToString())
+}
+
+void AStratPlayerState::Server_SetPlayerColor_Implementation(const FLinearColor& NewPlayerColor) { SetPlayerColor(NewPlayerColor); }
---
+++ Source/UE_RTS/Player/StratPlayerState.h
@@ -0,0 +1,41 @@
+﻿// Copyright Cody McCarty.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ModularPlayerState.h"
+#include "StratPlayerState.generated.h"
+
+/**
+ * todo doc
+ */
+UCLASS(meta=(PrioritizeCategories="User"))
+class UE_RTS_API AStratPlayerState : public AModularPlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AStratPlayerState();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void BeginReplication() override;
+	// virtual void ClientInitialize(AController* C) override;
+	// virtual void BeginPlay() override;
+
+	/** Player Color is a quick way other players identify another player. Can be used in text and decals. */
+	UFUNCTION(BlueprintPure, Category=StratPlayerState)
+	FLinearColor GetPlayerColor() const { return PlayerColor; }
+
+	/** Player Color is a quick way other players identify another player. Can be used in text and decals. */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category=StratPlayerState, meta=(AutoCreateRefTerm="NewPlayerColor"))
+	void SetPlayerColor(const FLinearColor& NewPlayerColor);
+
+protected:
+	/** Player Color is a quick way other players identify another player. Can be used in text and decals. */
+	UPROPERTY(EditInstanceOnly, ReplicatedUsing=OnRep_PlayerColor, Category="User|Options", Getter, Setter)
+	FLinearColor PlayerColor{FLinearColor::Gray};
+	UFUNCTION()
+	void OnRep_PlayerColor(const FLinearColor& OldPlayerColor);
+	void BroadcastPlayerColorChanged(const FLinearColor& OldPlayerColor);
+	UFUNCTION(Server, Reliable)
+	void Server_SetPlayerColor(const FLinearColor& NewPlayerColor);
+};
